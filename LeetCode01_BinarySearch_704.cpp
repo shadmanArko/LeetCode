418fb8a692486217351cb29fa9d33ad7e517908e
Shadman Arko <35429182+shadmanArko@users.noreply.github.com>
2022-05-23 00:48:20 +0600

    Create LeetCode01_BinarySearch_704.cpp

---
+++ LeetCode01_BinarySearch_704.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        
+        int l = 0;
+        int r = nums.size()-1;
+        while (l <= r) {
+        int m = l + (r - l) / 2;
+ 
+        // Check if x is present at mid
+        if (nums[m] == target)
+            return m;
+ 
+        // If x greater, ignore left half
+        if (nums[m] < target)
+            l = m + 1;
+ 
+        // If x is smaller, ignore right half
+        else
+            r = m - 1;
+    }
+ 
+    // if we reach here, then element was
+    // not present
+    return -1;
+        
+    }
+};
